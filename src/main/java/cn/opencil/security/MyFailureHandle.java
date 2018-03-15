1b43bf39debf258c2327109f81681ff14f334d58
Shaoqun Liu <liuinstein@163.com>
2018-03-15 19:00:27 +0800

    ADD: Restful response to Spring Security Authorization success&failure handle

--- src/main/java/cn/opencil/security/MyFailureHandle.java
+++ src/main/java/cn/opencil/security/MyFailureHandle.java
@@ -1,5 +1,6 @@
 package cn.opencil.security;
 
+import cn.opencil.vo.RestfulResult;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,16 +10,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class MyFailureHandle implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        RestfulResult result = new RestfulResult(1, e.getMessage(), new HashMap<>());
+        SecurityRestfulResponsePrinter responseHandle = new SecurityRestfulResponsePrinter();
+        responseHandle.print(request, response, result);
         // run to here when log in failed
         if (e instanceof InternalAuthenticationServiceException) {
             // an InternalAuthenticationServiceException will be threw when username is not existent
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } else {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
     }
 }
--- src/main/java/cn/opencil/vo/RestfulResult.java
+++ src/main/java/cn/opencil/vo/RestfulResult.java
@@ -1,5 +1,8 @@
 package cn.opencil.vo;
 
+import com.google.gson.GsonBuilder;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class RestfulResult {
@@ -11,7 +14,7 @@ public class RestfulResult {
     public RestfulResult(long code, String message, Map<String, Object> data) {
         this.code = code;
         this.message = message;
-        this.data = data;
+        this.data = data == null ? new HashMap<>() : data;
     }
 
     public long getCode() {
@@ -35,6 +38,41 @@ public class RestfulResult {
     }
 
     public void setData(Map<String, Object> data) {
-        this.data = data;
+        this.data = data == null ? new HashMap<>() : data;
+    }
+
+    /**
+     * Transfer this object to a JSON String
+     *
+     * @return a JSON String converted from this object
+     * @apiNote require com.google.gson dependency
+     */
+    public String toJsonString() {
+        return new GsonBuilder().enableComplexMapKeySerialization().create().toJson(this);
+    }
+
+    /**
+     * Transfer this object to an XML String
+     * it only provides limited ability to XML transformation.
+     * do NOT use it with a complex data structure and MAKE SURE all of the data objects have overwritten the toString method
+     * I wrote this only for Spring Security Authorization Handle Response
+     * You can use Spring REST in your SpringMVC project to transfer this to XML instead of using this method
+     *
+     * @return an empty string
+     */
+    public String toXmlString() {
+        StringBuilder result = new StringBuilder(200);
+        result.append("<RestfulResult>")
+                .append("<code>").append(code).append("</code>")
+                .append("<message>").append(message).append("</message>")
+                .append("<data>");
+        for (String key : data.keySet()) {
+            result.append("<").append(key).append(">")
+                    .append(data.get(key).toString())
+                    .append("</").append(key).append(">");
+        }
+        result.append("</data>")
+                .append("</RestfulResult>");
+        return result.toString();
     }
 }
