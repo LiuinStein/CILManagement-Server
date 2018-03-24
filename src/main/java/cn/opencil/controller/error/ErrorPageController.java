3d98a106627c27f2acd8be83903ea70f1dce2639
Shaoqun Liu <liuinstein@163.com>
2018-03-24 16:32:31 +0800

    ADD: error page controller and change the tomcat default error page

---
+++ src/main/java/cn/opencil/controller/error/ErrorPageController.java
@@ -0,0 +1,41 @@
+package cn.opencil.controller.error;
+
+import cn.opencil.vo.RestfulResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+@RestController
+public class ErrorPageController {
+
+    /**
+     * Handle all of the client errors (4xx) and the server errors (5xx)
+     * @param request the current http request
+     * @return RestfulResult
+     */
+    @RequestMapping("/error/")
+    public RestfulResult errorHandle(HttpServletRequest request) {
+        /*
+         * Constant Field Values for tomcat 9.0:
+         * https://tomcat.apache.org/tomcat-9.0-doc/servletapi/constant-values.html
+         */
+        String message = (String) request.getAttribute("javax.servlet.error.message");
+        int statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        return new RestfulResult(statusCode, message, new HashMap<>());
+    }
+
+    /**
+     * Handle the exceptions from other components except for controller (such as Spring Security)
+     * @param request the current http request
+     * @return RestfulResult
+     */
+    @RequestMapping("/exception/")
+    public RestfulResult exceptionHandle(HttpServletRequest request) {
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+        int statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        return new RestfulResult(statusCode, exception.getMessage(), new HashMap<>());
+    }
+
+}
