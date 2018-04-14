9242997b4657e6e8054d33d51b894501b9a27b55
Shaoqun Liu <liuinstein@163.com>
2018-04-14 15:31:31 +0800

    REFACTOR: the validation service

--- src/main/java/cn/opencil/service/implementation/MyValidationService.java
+++ src/main/java/cn/opencil/service/implementation/MyValidationService.java
@@ -1,25 +1,24 @@
 package cn.opencil.service.implementation;
 
 import cn.opencil.service.ValidationService;
-import com.shaoqunliu.validation.ValidationAdapter;
+import com.shaoqunliu.validation.AdaptersValidation;
 import com.shaoqunliu.validation.exception.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service("myValidationService")
 public class MyValidationService implements ValidationService {
 
-    private final ValidationAdapter validationAdapter;
+    private final AdaptersValidation adaptersValidation;
 
     @Autowired
-    public MyValidationService(@Qualifier("adaptersValidation") ValidationAdapter validationAdapter) {
-        this.validationAdapter = validationAdapter;
+    public MyValidationService(AdaptersValidation adaptersValidation) {
+        this.adaptersValidation = adaptersValidation;
     }
 
     @Override
     public <T> T validate(T object, Class<?>... groups) throws ValidationException {
-        return validationAdapter.validate(object, groups);
+        return adaptersValidation.validate(object, groups);
     }
 
 }
