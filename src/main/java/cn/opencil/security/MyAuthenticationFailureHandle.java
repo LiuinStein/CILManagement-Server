a00a1eae00a9009d57beec62a46b2c7fa9a43ab8
Shaoqun Liu <liuinstein@163.com>
2018-04-20 20:20:39 +0800

    FIXED: errors at authentication filter configuration

--- src/main/java/cn/opencil/security/MyAuthenticationFailureHandle.java
+++ src/main/java/cn/opencil/security/MyAuthenticationFailureHandle.java
@@ -1,7 +1,6 @@
 package cn.opencil.security;
 
 import cn.opencil.vo.RestfulResult;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
